9a20fdc9481b42064c89777d4b8a18a7e2b1de40
Alex Seezov <seezovcenko@gmail.com>
2017-05-21 14:37:16 +0300

    Main Turtle is completed

---
+++ TRUTLEGRAPH3/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TRUTLEGRAPH3
+{
+    public partial class MainForm : Form
+    {
+        Turtle turtle = new Turtle();
+
+        public MainForm()
+        {
+            InitializeComponent();
+            turtle.Angle = 0;
+            turtle.Display(DrawingArea);
+        }
+
+        private void MoveForwardButton_Click(object sender, EventArgs e)
+        {
+            turtle.MoveForward(MFTextBox.Text, PenCheckBox.Checked);
+        }
+
+        private void MoveBackwardsButton_Click(object sender, EventArgs e)
+        {
+            turtle.MoveBackwards(MBTextBox.Text, PenCheckBox.Checked);
+        }
+
+        private void TurnLeftButton_Click(object sender, EventArgs e)
+        {
+            turtle.TurnLeft(TLTextBox.Text);
+        }
+
+        private void TurnRightButton_Click(object sender, EventArgs e)
+        {
+            turtle.TurnRight(TRTextBox.Text);
+        }
+
+        private void DrawCircleButton_Click(object sender, EventArgs e)
+        {
+            turtle.DrawCircle(RadiusTextBox.Text,PenCheckBox.Checked);
+        }
+
+        private void DrawTriangleButton_Click(object sender, EventArgs e)
+        {
+            turtle.DrawTriangle(TriangleTextBox.Text, PenCheckBox.Checked);
+        }
+    }
+}
---
+++ TRUTLEGRAPH3/Turtle.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TRUTLEGRAPH3
+{
+    class Turtle
+    {
+        public PointF CurrentPoint;
+        public float Angle;
+        public Pen Pen = new Pen(Color.Black);
+        public Bitmap Bmp;
+        public Graphics G;
+        private PictureBox _drawingArea;
+
+
+
+        public Image buffer;
+
+        public void Display(PictureBox drawingArea)
+        {
+            
+            _drawingArea = drawingArea;
+            CurrentPoint = new PointF(drawingArea.Width / 2, drawingArea.Height / 2);
+            Bmp = new Bitmap(drawingArea.Width, drawingArea.Height);
+            drawingArea.Image = Bmp;
+            G = Graphics.FromImage(drawingArea.Image);
+            buffer = new Bitmap(drawingArea.Image);
+            DrawTurtle();
+            drawingArea.Invalidate();
+
+        }
+
+        private void DrawTurtle()
+        { 
+            G.DrawRectangle(Pen, CurrentPoint.X - 5, CurrentPoint.Y - 5, 10, 10);
+        }
+
+
+        public void MoveForward(string value, bool penIsActive)
+        {
+            G.Clear(Color.White);
+            G.DrawImage(buffer, 0, 0);
+            
+            PointF toMovePoint = new PointF
+            {
+                X = (float)(CurrentPoint.X + int.Parse(value) * Math.Cos(Angle / 360 * 2 * Math.PI)),
+                Y = (float)(CurrentPoint.Y + int.Parse(value) * Math.Sin(Angle / 360 * 2 * Math.PI))
+            };
+            
+            
+            if (penIsActive)
+            {
+                G.DrawLine(Pen, CurrentPoint, toMovePoint);
+            }
+
+            buffer = new Bitmap(_drawingArea.Image);
+            CurrentPoint = toMovePoint;
+
+            DrawTurtle();
+
+            _drawingArea.Invalidate();
+        }
+
+        public void MoveBackwards(string value, bool penIsActive)
+        {
+            G.Clear(Color.White);
+            G.DrawImage(buffer, 0, 0);
+
+            PointF toMovePoint = new PointF
+            {
+                X = (float)(CurrentPoint.X + int.Parse(value) * -Math.Cos(Angle / 360 * 2 * Math.PI)),
+                Y = (float)(CurrentPoint.Y + int.Parse(value) * -Math.Sin(Angle / 360 * 2 * Math.PI))
+            };
+
+
+            if (penIsActive)
+            {
+                G.DrawLine(Pen, CurrentPoint, toMovePoint);
+            }
+
+            buffer = new Bitmap(_drawingArea.Image);
+            CurrentPoint = toMovePoint;
+
+            DrawTurtle();
+
+            _drawingArea.Invalidate();
+        }
+
+        public void TurnLeft(string value)
+        {
+            Angle -= float.Parse(value);
+        }
+
+        public void TurnRight(string value)
+        {
+            Angle += float.Parse(value);
+        }
+
+        public void DrawCircle(string value, bool penIsActive)
+        {
+            for (int i = 0; i < 18; i++)
+            {
+                MoveForward(value,true);
+                Angle += 20f;
+            }
+            _drawingArea.Invalidate();
+        }
+
+        public void DrawTriangle(string value, bool penIsActive)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                MoveForward(value, true);
+                Angle += 120f;
+            }
+            _drawingArea.Invalidate();
+        }
+    }
+}
